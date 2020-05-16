4be41475e11a888455518118e1461a60811310a4
Fuma_Tsuchiya <korezonzip@gmail.comm>
2020-05-16 19:36:25 +0900

    create update, delete

--- app/controllers/tweet/TweetController.scala
+++ app/controllers/tweet/TweetController.scala
@@ -28,7 +28,9 @@ class TweetController @Inject()(val controllerComponents: ControllerComponents) 
   def store(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
     //fold: データ受け取りの成功・失敗を分岐しつつ処理を行える
     //fold()(): LeftとRightの処理
-    //bindFromRequest: implicitでreqを受け取る
+    //bindFromRequest: implicitでrequestを受け取る
+    //そのため、リクエスト情報からformで設定したマッピング情報を元に入力チェックと値変換を行える
+    //そして、その処理の成否によってfoldで処理を分岐しているという動き
     form.bindFromRequest().fold(
       //処理失敗時の処理
       (formWithErrors: Form[TweetFormData]) => {
@@ -52,6 +54,51 @@ class TweetController @Inject()(val controllerComponents: ControllerComponents) 
     tweets.find(_.id.exists(_ == id)) match {
       case Some(tweet) => Ok(views.html.tweet.show(tweet))
       case None        => NotFound(views.html.error.page404())
+    }
+  }
+
+  /*編集画面を開く*/
+  def edit(id: Long) = Action { implicit request: Request[AnyContent] =>
+    tweets.find(_.id.exists(_ == id)) match {
+      case Some(tweet) =>
+        Ok(views.html.tweet.edit(
+          //データを識別するために
+          id,
+          //fill: formに値を入れる
+          form.fill(TweetFormData(tweet.content))
+        ))
+      case None    =>
+        NotFound(views.html.error.page404())
+    }
+  }
+
+  /*対象のツイートを更新する*/
+  def update(id: Long) = Action { implicit request: Request[AnyContent] =>
+    form.bindFromRequest().fold(
+      (formWithErrors: Form[TweetFormData]) => {
+        BadRequest(views.html.tweet.edit(id, formWithErrors))
+      },
+      (data: TweetFormData) => {
+        tweets.find(_.id.exists(_ == id)) match{
+          case Some(tweet) =>
+            tweets.update(id.toInt -1, tweet.copy(content = data.content))
+            Redirect(routes.TweetController.list())
+          case None =>
+            NotFound(views.html.error.page404())
+        }
+      }
+    )
+  }
+
+  def delete() = Action { implicit request: Request[AnyContent] =>
+    val idOpt =request.body.asFormUrlEncoded.get("id").headOption
+    tweets.find(_.id.map(_.toString) == idOpt) match {
+      case Some(tweet) =>
+        tweets -= tweet
+        //削除 -> リダイレクト
+        Redirect(routes.TweetController.list())
+      case None =>
+        NotFound(views.html.error.page404())
     }
   }
 }
